910352cfd3351f3a3c6360de948238748a53e355
Jeeva-KM <jeevamurugesan123@gmail.com>
2026-06-28 18:32:22 +0530

    Week 1 - JUnit Exercise 1

---
+++ Week1/JUnit-Exercise 1/CalculatorTest.java
@@ -0,0 +1,12 @@
+package com.cognizant.junit;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    void testAdd() {
+        System.out.println("JUnit setup is successful.");
+    }
+
+}
