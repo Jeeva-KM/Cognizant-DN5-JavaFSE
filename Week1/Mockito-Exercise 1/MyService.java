28dae7d029eb94f7d69dc4a08b9bb6388aa2b78d
Jeeva-KM <jeevamurugesan123@gmail.com>
2026-06-28 19:17:18 +0530

    Week 1 - Mockito Exercise 1

---
+++ Week1/Mockito-Exercise 1/MyService.java
@@ -0,0 +1,11 @@
+package com.cognizant.mockito;
+
+public class MyService {
+    private ExternalApi externalApi;
+    public MyService(ExternalApi externalApi) {
+        this.externalApi = externalApi;
+    }
+    public String fetchData() {
+        return externalApi.getData();
+    }
+}
