e00012c1fa27a46aab8f6e8d6eb04da06c58b6f2
Jeeva-KM <jeevamurugesan123@gmail.com>
2026-06-28 17:34:27 +0530

    Week 1 - PL/SQL exercise 1

---
+++ Week1/PL SQL-Exercise 1/plsqlexercise1.sql
@@ -0,0 +1,239 @@
+CREATE TABLE Customers (
+    CustomerID NUMBER PRIMARY KEY,
+    Name VARCHAR2(100),
+    DOB DATE,
+    Balance NUMBER,
+    LastModified DATE
+);
+
+CREATE TABLE Accounts (
+    AccountID NUMBER PRIMARY KEY,
+    CustomerID NUMBER,
+    AccountType VARCHAR2(20),
+    Balance NUMBER,
+    LastModified DATE,
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+);
+
+CREATE TABLE Transactions (
+    TransactionID NUMBER PRIMARY KEY,
+    AccountID NUMBER,
+    TransactionDate DATE,
+    Amount NUMBER,
+    TransactionType VARCHAR2(20),
+    FOREIGN KEY (AccountID) REFERENCES Accounts(AccountID)
+);
+
+CREATE TABLE Loans (
+    LoanID NUMBER PRIMARY KEY,
+    CustomerID NUMBER,
+    LoanAmount NUMBER,
+    InterestRate NUMBER,
+    StartDate DATE,
+    EndDate DATE,
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+);
+
+CREATE TABLE Employees (
+    EmployeeID NUMBER PRIMARY KEY,
+    Name VARCHAR2(100),
+    Position VARCHAR2(50),
+    Salary NUMBER,
+    Department VARCHAR2(50),
+    HireDate DATE
+);
+SELECT table_name
+FROM user_tables
+ORDER BY table_name;
+INSERT INTO Customers (CustomerID, Name, DOB, Balance, LastModified)
+VALUES (1, 'John Doe', TO_DATE('1985-05-15', 'YYYY-MM-DD'), 1000, SYSDATE);
+
+INSERT INTO Customers (CustomerID, Name, DOB, Balance, LastModified)
+VALUES (2, 'Jane Smith', TO_DATE('1990-07-20', 'YYYY-MM-DD'), 1500, SYSDATE);
+
+INSERT INTO Accounts (AccountID, CustomerID, AccountType, Balance, LastModified)
+VALUES (1, 1, 'Savings', 1000, SYSDATE);
+
+INSERT INTO Accounts (AccountID, CustomerID, AccountType, Balance, LastModified)
+VALUES (2, 2, 'Checking', 1500, SYSDATE);
+
+INSERT INTO Transactions (TransactionID, AccountID, TransactionDate, Amount, TransactionType)
+VALUES (1, 1, SYSDATE, 200, 'Deposit');
+
+INSERT INTO Transactions (TransactionID, AccountID, TransactionDate, Amount, TransactionType)
+VALUES (2, 2, SYSDATE, 300, 'Withdrawal');
+
+INSERT INTO Loans (LoanID, CustomerID, LoanAmount, InterestRate, StartDate, EndDate)
+VALUES (1, 1, 5000, 5, SYSDATE, ADD_MONTHS(SYSDATE, 60));
+
+INSERT INTO Employees (EmployeeID, Name, Position, Salary, Department, HireDate)
+VALUES (1, 'Alice Johnson', 'Manager', 70000, 'HR',
+TO_DATE('2015-06-15', 'YYYY-MM-DD'));
+
+INSERT INTO Employees (EmployeeID, Name, Position, Salary, Department, HireDate)
+VALUES (2, 'Bob Brown', 'Developer', 60000, 'IT',
+TO_DATE('2017-03-20', 'YYYY-MM-DD'));
+
+COMMIT;
+SELECT * FROM Customers;
+SELECT * FROM Loans;
+INSERT INTO Customers
+VALUES
+(
+3,
+'Robert',
+TO_DATE('1950-05-15','YYYY-MM-DD'),
+20000,
+SYSDATE
+);
+
+INSERT INTO Loans
+VALUES
+(
+2,
+3,
+10000,
+8,
+SYSDATE,
+ADD_MONTHS(SYSDATE,24)
+);
+
+COMMIT;
+BEGIN
+
+    FOR c IN (SELECT * FROM Customers)
+    LOOP
+
+        IF MONTHS_BETWEEN(SYSDATE, c.DOB) / 12 > 60 THEN
+
+            UPDATE Loans
+            SET InterestRate = InterestRate - 1
+            WHERE CustomerID = c.CustomerID;
+
+        END IF;
+
+    END LOOP;
+
+    COMMIT;
+
+END;
+SELECT LoanID,
+       CustomerID,
+       InterestRate
+FROM Loans
+ORDER BY LoanID;
+SELECT * FROM Customers;
+INSERT INTO Customers
+(CustomerID, Name, DOB, Balance, LastModified)
+VALUES
+(
+3,
+'Robert',
+TO_DATE('1950-05-15','YYYY-MM-DD'),
+20000,
+SYSDATE
+);
+INSERT INTO Loans
+(LoanID, CustomerID, LoanAmount, InterestRate, StartDate, EndDate)
+VALUES
+(
+2,
+3,
+10000,
+8,
+SYSDATE,
+ADD_MONTHS(SYSDATE,24)
+);
+COMMIT;
+SELECT * FROM Loans;
+BEGIN
+
+    FOR c IN (SELECT * FROM Customers)
+    LOOP
+
+        IF MONTHS_BETWEEN(SYSDATE, c.DOB) / 12 > 60 THEN
+
+            UPDATE Loans
+            SET InterestRate = InterestRate - 1
+            WHERE CustomerID = c.CustomerID;
+
+        END IF;
+
+    END LOOP;
+
+    COMMIT;
+
+END;
+SELECT LoanID,
+       CustomerID,
+       InterestRate
+FROM Loans
+ORDER BY LoanID;
+
+-- Scenario 2
+ALTER TABLE Customers
+ADD IsVIP VARCHAR2(5);
+BEGIN
+
+    FOR c IN (SELECT * FROM Customers)
+    LOOP
+
+        IF c.Balance > 10000 THEN
+
+            UPDATE Customers
+            SET IsVIP = 'TRUE'
+            WHERE CustomerID = c.CustomerID;
+
+        END IF;
+
+    END LOOP;
+
+    COMMIT;
+
+END;
+SELECT CustomerID,
+       Name,
+       Balance,
+       IsVIP
+FROM Customers
+ORDER BY CustomerID;
+
+-- scenario 3
+
+INSERT INTO Loans
+(
+    LoanID,
+    CustomerID,
+    LoanAmount,
+    InterestRate,
+    StartDate,
+    EndDate
+)
+VALUES
+(
+    3,
+    1,
+    2000,
+    6,
+    SYSDATE,
+    SYSDATE + 10
+);
+
+COMMIT;
+BEGIN
+
+    FOR l IN (
+        SELECT *
+        FROM Loans
+        WHERE EndDate BETWEEN SYSDATE AND SYSDATE + 30
+    )
+    LOOP
+
+        DBMS_OUTPUT.PUT_LINE(
+            'Reminder: Loan ID ' || l.LoanID ||
+            ' is due on ' || TO_CHAR(l.EndDate, 'DD-MON-YYYY')
+        );
+
+    END LOOP;
+
+END;
